0b0a8e38d2bfa9a5d35a6f3ba15cd1d4f612ee98
marco_ <elom.karl.patrick@gmail.com>
2026-09-06 03:48:30 +0200

    fix: remove config import cycle and correct effective batch size

--- lapis/config/training_config.py
+++ lapis/config/training_config.py
@@ -1,30 +1,33 @@
+from __future__ import annotations
+
 from lapis.config.base import load_config
-from lapis.config import get_default_config
+
+
+def _default_config():
+    return load_config("configs/tiny.yaml")
 
 
 class TrainingConfig:
     def __init__(self, config=None):
-        if config is None:
-            config = get_default_config()
-        
-        self.learning_rate = float(config["training"]["learning_rate"])
-        self.weight_decay = float(config["training"]["weight_decay"])
-        self.warmup_steps = int(config["training"]["warmup_steps"])
-        self.max_steps = int(config["training"]["max_steps"])
-        self.batch_size = int(config["training"]["batch_size"])
-        self.micro_batch_size = int(config["training"]["micro_batch_size"])
-        self.gradient_accumulation_steps = int(config["training"]["gradient_accumulation_steps"])
-        self.gradient_clip = float(config["training"]["gradient_clip"])
-    
+        config = _default_config() if config is None else config
+        training = config["training"]
+        self.learning_rate = float(training["learning_rate"])
+        self.weight_decay = float(training["weight_decay"])
+        self.warmup_steps = int(training["warmup_steps"])
+        self.max_steps = int(training["max_steps"])
+        self.batch_size = int(training["batch_size"])
+        self.micro_batch_size = int(training["micro_batch_size"])
+        self.gradient_accumulation_steps = int(training["gradient_accumulation_steps"])
+        self.gradient_clip = float(training["gradient_clip"])
+
     def get_effective_batch_size(self):
-        return self.batch_size * self.gradient_accumulation_steps
+        return self.micro_batch_size * self.gradient_accumulation_steps
 
 
 class DataConfig:
     def __init__(self, config=None):
-        if config is None:
-            config = get_default_config()
-        
-        self.dataset_name = config.get("data", {}).get("dataset_name", "lapis")
-        self.max_seq_length = int(config.get("data", {}).get("max_seq_length", 512))
-        self.shard_size = int(config.get("data", {}).get("shard_size", 1024 * 1024))
+        config = _default_config() if config is None else config
+        data = config.get("data", {})
+        self.dataset_name = data.get("dataset_name", "lapis")
+        self.max_seq_length = int(data.get("max_seq_length", 512))
+        self.shard_size = int(data.get("shard_size", 1024 * 1024))
