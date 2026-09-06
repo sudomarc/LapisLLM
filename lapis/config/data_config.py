ae553199b9e6d02e464de68ca764e27c78db72d9
marco_ <elom.karl.patrick@gmail.com>
2026-09-06 04:28:25 +0200

    fix: repair standalone DataConfig import

--- lapis/config/data_config.py
+++ lapis/config/data_config.py
@@ -1,10 +1,10 @@
-from lapis.config.base import load_config
+from lapis.config.model_config import get_default_config
+
 
 class DataConfig:
     def __init__(self, config=None):
-        if config is None:
-            config = get_default_config()
-        
-        self.dataset_name = config.get("data", {}).get("dataset_name", "lapis")
-        self.max_seq_length = config.get("data", {}).get("max_seq_length", 512)
-        self.shard_size = config.get("data", {}).get("shard_size", 1024 * 1024)
+        config = get_default_config() if config is None else config
+        data = config.get("data", {})
+        self.dataset_name = data.get("dataset_name", "lapis")
+        self.max_seq_length = int(data.get("max_seq_length", 512))
+        self.shard_size = int(data.get("shard_size", 1024 * 1024))
